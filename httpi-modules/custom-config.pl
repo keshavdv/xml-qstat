96646a31ea0a1e8797bc047d68aaf9c6c54d3cc3
Mark Olesen <Mark.Olesen@emconTechnologies.com>
2009-11-08 15:33:19 +0100

    bugfix: make-httpi wasn't interpolating ~/ as a user might expect.

--- httpi-modules/custom-config.pl
+++ httpi-modules/custom-config.pl
@@ -2,38 +2,41 @@
 # It hooks into the configure system provided by HTTPi-1.6.2
 
 # get xmlqstat root
-$DEF_XMLQSTAT_ROOT = &prompt(<<"EOF", '$ENV{HOME}/xml-qstat', 1);
+$DEF_XMLQSTAT_ROOT = &interprompt(<<"EOF", '~/xml-qstat', 1, \&inter_homedir);
 
 xml-qstat customization:
 ------------------------
 Specify where the xml-qstat root is located.
 The 'xmlqstat' directory under this root will be served by HTTPi
 as the resource '/xmlqstat'.
+
+You can use Perl variables for this option (example: \$ENV{'HOME'}/xml-qstat).
+As a shortcut, ~/ in first position will be turned into \$ENV{'HOME'}/,
+which is "$ENV{'HOME'}/".
+
+xml-qstat resource root:?
 EOF
 
 # verify directory plausibility
+for ($DEF_XMLQSTAT_ROOT)
 {
-    my $root;
-
-    eval qq{\$root = "$DEF_XMLQSTAT_ROOT"};
-
     if (
-            not $@
-        and -d $root
-        and -d "$root/xmlqstat"
-        and -d "$root/xmlqstat/css"
-        and -d "$root/xmlqstat/xsl"
+        -d $_
+        and -d "$_/xmlqstat"
+        and -d "$_/xmlqstat/css"
+        and -d "$_/xmlqstat/xsl"
       )
     {
         print <<"EOF";
-Okay. Seems to have the correct xml-qstat directory structure
+Okay. The directory '$_'
+Seems to have the correct xml-qstat directory structure.
 
 EOF
     }
     else {
         print <<"EOF";
-WARNING: That directory has not been created or does not appear to contain
-the xml-qstat directories!!
+WARNING: The directory '$_'
+Does not appear to contain the correct xml-qstat directory structure!!
 
 EOF
     }

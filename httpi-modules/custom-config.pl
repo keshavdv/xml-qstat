cc0cda23e09c946107f1453bbbad792e5da7e656
Mark Olesen <Mark.Olesen@emconTechnologies.com>
2009-11-09 17:45:04 +0100

    Attempt httpi-xmlqstat for non-Linux architectures
    Configuration:
    - use GridEngine <sge_root>/util/arch to determine the system
      architecture and the name of the dynamic library env.
      Otherwise prompt for user input.
    
    Runtime:
    - set lib Env to <sge_root>/lib/<arch> before calling the GridEngine
      commands

--- httpi-modules/custom-config.pl
+++ httpi-modules/custom-config.pl
@@ -1,6 +1,83 @@
 # This file contains custom questions for configuring the HTTPi-xmlqstat
 # It hooks into the configure system provided by HTTPi-1.6.2
 
+
+# get GridEngine arch/lib information
+print "GridEngine configuration\n";
+print "Trying to determine the architecture and library requirements\n";
+
+( $DEF_SGE_ARCH, $DEF_LIBENV ) = ( '', '' );
+
+if ( $ENV{SGE_ROOT} and -d $ENV{SGE_ROOT} ) {
+    my $archScript = "$ENV{SGE_ROOT}/util/arch";
+    # use 'arch' script if possible
+
+    print "checking what '$archScript' reports...\n";
+    chomp( $DEF_SGE_ARCH = qx{$archScript 2>/dev/null} );
+    if ($DEF_SGE_ARCH)
+    {
+        chomp( $DEF_LIBENV = qx{$archScript -lib 2>/dev/null} );
+    }
+    else
+    {
+        print "Hmm. Couldn't seem to execute $archScript\n";
+    }
+}
+
+
+if ($DEF_SGE_ARCH) {
+    print "Using $DEF_SGE_ARCH for the GridEngine architecture\n";
+}
+else {
+    print
+"Couldn't get GridEngine architecture automatically, need to do it ourselves\n";
+
+    $DEF_SGE_ARCH = "undef";
+
+    while (1) {
+        $DEF_SGE_ARCH = &prompt( <<"EOF", $DEF_SGE_ARCH, 1 );
+
+Define the architecture used for the GridEngine commands.
+This should correspond to the value emitted by the \$SGE_ROOT/util/arch
+except that we couldn't seem to find that script.
+
+Hint: the 'arch' value here should allow us to find
+\$SGE_ROOT/bin/<arch>/qstat
+
+Which value should be used for the GridEngine architecture?
+EOF
+
+        if ( $DEF_SGE_ARCH eq "undef" or not $DEF_SGE_ARCH ) {
+            print <<"EOF";
+The GridEngine architecture value '$DEF_SGE_ARCH' looks implausible
+Please try again ...
+
+EOF
+        }
+        else {
+            last;
+        }
+    }
+}
+
+
+# library path setting for architectures where RUNPATH is not supported
+if ( $DEF_SGE_ARCH =~ /^(lx|sol)/ or $DEF_SGE_ARCH eq "hp11-64" ) {
+    print "Good. We can apparently use RUNPATH for this architecture!\n";
+    $DEF_LIBENV = '';
+}
+else
+{
+    $DEF_LIBENV = &prompt(<<"EOF", 'LD_LIBRARY_PATH', 1);
+
+Define the name of the dynamic link library environment variable
+for your machine. This is needed to load the GridEngine libraries.
+
+Which environment variable is used for the libraries?
+EOF
+}
+
+
 # get xmlqstat root
 $DEF_XMLQSTAT_ROOT = &interprompt(<<"EOF", '~/xml-qstat', 1, \&inter_homedir);
 
